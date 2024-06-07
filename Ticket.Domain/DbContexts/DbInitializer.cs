f5ddde585c69381a5b25d396b5630d4ad6d93f9b
riconokvip <ricon@okvip.com>
2024-06-07 22:27:03 +0700

    add seeder

---
+++ Ticket.Domain/DbContexts/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿namespace Ticket.Domain.DbContexts
+{
+    public class DbInitializer
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DbInitializer(ApplicationDbContext applicationDb)
+        {
+            _context = applicationDb;
+        }
+
+        public void Seeding()
+        {
+            if (!_context.Users.Any())
+            {
+                var seeder = Seeder();
+                _context.Add(seeder);
+                _context.SaveChanges();
+            }
+        }
+
+        private UserEntities Seeder()
+        {
+            var admin = new UserEntities
+            {
+                Id = Guid.NewGuid().ToString(),
+                WorkName = "Quản trị viên",
+                Email = "admin@okvip.com",
+                Level = "Quản trị viên",
+                SecurityStamp = new Guid().ToString("D"),
+                PasswordHash = "adminokvip".Hash(),
+                CreatedAt = ApplicationExtensions.NOW
+            };
+            return admin;
+        }
+    }
+}
